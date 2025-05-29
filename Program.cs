03eff9476ae6daa1b5d7779b0ee43a320ce8f10e
rutujajadhav00 <rutujajadhav413512@gmail.com>
2025-05-29 22:23:31 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace reverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "My name is rutuja";
+
+
+            for (int i = s.Length - 1; i >= 0; i--) 
+            {
+                Console.Write(s[i]);
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+
+
+        }
+    }
+}
